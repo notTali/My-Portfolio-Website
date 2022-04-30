09a3e9f47ca5c3074a572042ad7d77d72a5ad001
notTali <mtalifhani54@gmail.com>
2022-04-30 06:42:01 +0200

    Initialize Particle js

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    Particles.init({
+      selector: ".particles",
+      color: ['#5FD1D4'],
+      connectParticles: true,
+      maxParticles: 90,
+      sizeVariations: 3,
+      speed: 0.7,
+    });
+};
+
