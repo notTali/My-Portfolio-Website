0d780b99eb9eb0db6d1a3f138b9b87ad93096653
notTali <mtalifhani54@gmail.com>
2022-04-30 11:02:29 +0200

    Class for left and right subtitles

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ window.onload = function() {
     Particles.init({
       selector: ".particles",
       color: ['#5FD1D4'],
-      connectParticles: true,
-      maxParticles: 90,
-      sizeVariations: 3,
+      connectParticles: false,
+      maxParticles: 70,
+      sizeVariations: 30,
       speed: 0.7,
     });
 };
